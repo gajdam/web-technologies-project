50b24f1765821412a1ffc6d37982dfdd2cf7dab1
Maciej Gajda <gajdam11@gmail.com>
2024-01-24 12:40:39 +0100

    logger added

--- login.php
+++ login.php
@@ -27,14 +27,28 @@ class UserLogin {
                 session_start();
                 $_SESSION['login'] = $row['id'];
                 $_SESSION['is_admin'] = $row['is_admin'];
+                
+                // Logowanie powiodło się
+                $this->logger('Successful login for user: ' . $login);
+                
                 header('Location: main_page.php?flag=true');
                 exit;
             } else {
+                // Błąd logowania
+                $this->logger('Failed login attempt for user: ' . $login);
                 echo 'Błędne dane logowania';
             }
         } else {
+            // Błąd logowania
+            $this->logger('Failed login attempt for user: ' . $login);
             echo 'Błędne dane logowania';
         }
+    }
+    
+    public function logger($message) {
+        $logFile = 'login_logs.txt';
+        $logData = date('Y-m-d H:i:s') . ' - ' . $message . PHP_EOL;
+        file_put_contents($logFile, $logData, FILE_APPEND);
     }
 }
 
